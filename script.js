8f9b73b70cf64bb1339f2f54df060f563ce1d474
prashant22r <mainiprashant3@gmail.com>
2024-09-01 20:04:16 +0530

    Project

--- script.js
+++ script.js
@@ -31,3 +31,16 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 }
 loco();
+let nav_list = document.getElementById('nav-list')
+let openMenu = document.querySelector('.menuopen')
+let cross = document.querySelector('.cross')
+openMenu.addEventListener('click',()=>{
+    nav_list.style.opacity = '100%'
+    openMenu.style.display ='none'
+})
+cross.addEventListener('click',()=>{
+  nav_list.style.opacity = '0%';
+  setTimeout(()=>{
+    openMenu.style.display ='block'
+  },400)
+})
